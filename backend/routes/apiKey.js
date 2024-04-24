453a5e9adb8c6550c958efeb0f0d2f65ab18ce69
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-24 17:41:00 +0530

    backend/ alterations and addition of calculating api requests

--- backend/routes/apiKey.js
+++ backend/routes/apiKey.js
@@ -1,65 +1,71 @@
 const express = require("express");
 const router = express.Router();
 const fetchUserId = require("../middleware/fetchUserId");
-const crypto = require('crypto');
-const Data = require("../models/Data");
+const crypto = require("crypto");
 const User = require("../models/User");
 
 // Route: to fetch existing data.
 router.put("/createAPI", fetchUserId, async (req, res) => {
-    try {
-      // Generate a random API key
-      const apiKey = crypto.randomBytes(32).toString("hex"); // Generates a 64-character hexadecimal string (32 bytes)
-  
-      // Update the user document with the generated API key
-      const userDocument = await User.findByIdAndUpdate(req.userId, {
-        $set: { apiKeys: apiKey },
-      },{new : true}).select([ "-password" ]);
-      
-      if (!userDocument) {
-        return res.status(404).send("User not found");
-      }
-      console.log("userdocument =",userDocument)
-      res.send({userDocument});
-    } catch (error) {
-      console.error("Error creating API key:", error);
-      res.status(500).send("Internal server error");
-    }
-  });
+  try {
+    // Generate a random API key
+    const apiKey = crypto.randomBytes(32).toString("hex"); // Generates a 64-character hexadecimal string (32 bytes)
 
-  // Route to delete the api key.
-  router.put("/deleteAPI", fetchUserId, async (req, res) => {
-    try {
-      // Update the user document with the undefined API key
-      const userDocument = await User.findByIdAndUpdate(req.userId, {
+    // Update the user document with the generated API key
+    const userDocument = await User.findByIdAndUpdate(
+      req.userId,
+      {
+        $set: { apiKeys: apiKey },
+      },
+      { new: true }
+    ).select(["-password"]);
+
+    if (!userDocument) {
+      return res.status(404).send("User not found");
+    }
+    res.send({ userDocument });
+  } catch (error) {
+    console.error("Error creating API key:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// Route to delete the api key.
+router.put("/deleteAPI", fetchUserId, async (req, res) => {
+  try {
+    // Update the user document with the undefined API key
+    const userDocument = await User.findByIdAndUpdate(
+      req.userId,
+      {
         $set: { apiKeys: null },
-      },{new : true}).select(["-password"]);
-  
-      if (!userDocument) {
-        return res.status(404).send("User not found");
-      }
-      
-      await userDocument.save();
-      res.send("Api key deleted successfully");
-    } catch (error) {
-      console.error("Error deleting API key:", error);
-      res.status(500).send("Internal server error");
+      },
+      { new: true }
+    ).select(["-password"]);
+
+    if (!userDocument) {
+      return res.status(404).send("User not found");
     }
-  });
-  
-  // Route to fetch user api key.
-  router.get("/APIKey", fetchUserId, async (req, res) => {
-    try {
-      // Update the user document with the undefined API key
-      const userDocument = await User.findById(req.userId).select(["-password"]);
-      if (!userDocument) {
-        return res.status(404).send("User not found");
-      }
-      res.send({userDocument});
-    } catch (error) {
-      console.error("Error fetching API key:", error);
-      res.status(500).send("Internal server error");
+
+    await userDocument.save();
+    res.send("Api key deleted successfully");
+  } catch (error) {
+    console.error("Error deleting API key:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// Route to fetch user api key.
+router.get("/APIKey", fetchUserId, async (req, res) => {
+  try {
+    // Update the user document with the undefined API key
+    const userDocument = await User.findById(req.userId).select(["-password"]);
+    if (!userDocument) {
+      return res.status(404).send("User not found");
     }
-  });
-  
+    res.send({ userDocument });
+  } catch (error) {
+    console.error("Error fetching API key:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
 module.exports = router;
--- backend/routes/data.js
+++ backend/routes/data.js
@@ -3,26 +3,53 @@ const router = express.Router();
 const fetchUserId = require("../middleware/fetchUserId");
 const Data = require("../models/Data");
 const User = require("../models/User");
-
+const dayjs = require("dayjs");
+const Request = require("../models/RequestLog");
 
 // Route: to fetch existing data.
 router.get("/fetchData/:id", fetchUserId, async (req, res) => {
   try {
-    const foundTask = await User.findOne({apiKeys : req.params.id});
-    if (!foundTask) {
-      return res.status(404).send("user is not available");
+    const user = await User.findOne({ apiKeys: req.params.id });
+    if (!user) {
+      return res.status(404).send("api key is not available");
     }
 
     const existingData = await Data.find();
     if (!existingData) {
-        return res.status(404).send("data is not available");
-      }
-    res.send({existingData});
+      return res.status(404).send("data is not available");
+    }
+
+    const dailyRequestCount = await Request.countDocuments({
+      user_id: req.userId,
+      timestamp: { $gte: dayjs().startOf("day").toDate() },
+    });
+    console.log("let seee daily counts = ", dailyRequestCount);
+
+    if (dailyRequestCount >= 5) {
+      return res.status(429).json({ error: "Daily request limit exceeded" });
+    }
+
+    // Check monthly limit
+    const monthlyRequestCount = await Request.countDocuments({
+      user_id: req.userId,
+      timestamp: { $gte: dayjs().startOf("month").toDate() },
+    });
+    console.log("monthly addups =  ", monthlyRequestCount);
+    if (monthlyRequestCount >= 10) {
+      return res.status(429).json({ error: "Monthly request limit exceeded" });
+    }
+
+    await Request.create({
+      user_id: req.userId,
+      endpoint: req.path,
+      timeStamp: dayjs().toDate(),
+    });
+
+    res.send({ existingData });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal server error");
   }
 });
-
 
 module.exports = router;
