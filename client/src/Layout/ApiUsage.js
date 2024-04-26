d230e6799392809da9d444096d47ad7534ad4c08
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-26 18:43:47 +0530

    Layout : API Usage Statistics

---
+++ client/src/Layout/ApiUsage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import EndDate from "../components/DateUse/lastday";
+import FirstDate from "../components/DateUse/firstdate";
+import TotalRequests from "../components/DateUse/totalrequests";
+
+export default function ApiUsage() {
+  return (
+    <div className='my-10 mb-36'>
+      <h1 className="text-center text-2xl font-bold mb-4">API Usage Statistics</h1>
+      <div className='flex flex-wrap justify-around'>
+        <FirstDate />
+        <EndDate />
+        <TotalRequests />
+      </div>
+    </div>
+  );
+}
